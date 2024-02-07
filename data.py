d3bf10bbf3c862c48c2e0207e67ea902cd60e5cf
hyacinths147 <caterynac@nycstudents.net>
2024-02-07 13:57:07 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,12 @@
-bill = int(input("money spent"))
+""" bill = float(30)
 tip = int(input("extra money paid"))
 print(bill+tip)
+ """
+
+""" number = int(input("number"))
+if number%2 == 1:
+    print("odd")
+else:
+    print("even") """
+
+bill = int(input("money spent on food"))
